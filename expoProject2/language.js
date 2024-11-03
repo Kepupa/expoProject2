778c25991fbba00704320266ffc6e47aaacd836a
alek <2Sa1kle2@mail.ru>
2024-11-03 21:15:00 +0300

    update language

--- expoProject2/language.js
+++ expoProject2/language.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 
+
 const LanguageItem = ({ lang, experience, logo }) => {
-  return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: logo }} />
-      <View>
-        <Text style={styles.langText}>{lang}</Text>
-        <Text style={styles.expText}>
-          Опыт: {experience.years} лет {experience.months} месяцев
-        </Text>
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={{ uri: logo }} />
+            <View>
+                <Text style={styles.langText}>{lang}</Text>
+                {experience.years >= 1 ? (
+                    <Text style={styles.expText}>
+                        Опыт: {experience.years} {experience.years === 1 ? 'год' : 'года'}, {experience.months} месяца
+                    </Text>
+                ) : (
+                    <Text style={styles.expText}>
+                        Опыт: {experience.months} месяца
+                    </Text>
+                )}
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
